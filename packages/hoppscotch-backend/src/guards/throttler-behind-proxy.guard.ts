d0d661b73c6cbed30be9eede24216bbcb6d680fb
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2025-08-12 10:02:43 +0200

    chore: debug

--- packages/hoppscotch-backend/src/guards/throttler-behind-proxy.guard.ts
+++ packages/hoppscotch-backend/src/guards/throttler-behind-proxy.guard.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected async getTracker(req: Record<string, any>): Promise<string> {
+    console.log(JSON.stringify(req.ips));
+
     return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
   }
 }
